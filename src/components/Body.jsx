359d8bb39c6d2d6601d64ea9eb1ccdb67e89572f
Satyam Mittal <mittal.satyam9058@gmail.com>
2024-07-17 15:28:17 +0530

    adjust text-size

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -4,10 +4,10 @@ import Products from "./Products";
 function Body() {
   return (
     <div className="w-full flex p-2">
-      <div className="w-[25%]">
+      <div className="w-[30%] md:w-[25%]">
         <Sidebar/>
       </div>
-      <div className="w-[75%]">
+      <div className="w-[70%] md:w-[75%]">
         <Products />
       </div>
     </div>
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,15 +1,15 @@
 function Sidebar() {
   return (
     <div className="bg-white shadow-lg h-full p-2">
-      <h2 className="text-xl font-semibold my-2 text-center">Categories</h2>
+      <h2 className="text-sm md:text-xl font-semibold my-2 text-center">Categories</h2>
       <ul className="space-y-2">
-        <li className="w-full text-left p-2 rounded-lg hover:bg-gray-300">
+        <li className="text-xs md:text-base w-full text-left p-2 rounded-lg hover:bg-gray-300">
           Electronics
         </li>
-        <li className="w-full text-left p-2 rounded-lg hover:bg-gray-300">
+        <li className="text-xs md:text-base w-full text-left p-2 rounded-lg hover:bg-gray-300">
           Books
         </li>
-        <li className="w-full text-left p-2 rounded-lg hover:bg-gray-300">
+        <li className="text-xs md:text-base w-full text-left p-2 rounded-lg hover:bg-gray-300">
           Clothing
         </li>
       </ul>
